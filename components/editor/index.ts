5cf0ed536741b8e7def3d2a63237ea1c85bc1e99
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-14 08:55:41 -0500

    feat: Add editor components index export

---
+++ components/editor/index.ts
@@ -0,0 +1,6 @@
+// CRAV Scrapbook - Editor Components Export
+export { EditorCanvas } from './EditorCanvas';
+export { EditorToolbar } from './EditorToolbar';
+export { AssetsPanel } from './AssetsPanel';
+export { PropertiesPanel } from './PropertiesPanel';
+export { PagesPanel } from './PagesPanel';
